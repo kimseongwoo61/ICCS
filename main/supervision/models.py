9ce77acb4168a364e6f5b26e59e67412fa1c1290
K.S.W <52404779+kimseongwoo61@users.noreply.github.com>
2023-12-28 23:31:16 +0900

    Delete main directory

--- main/supervision/models.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
-from django.db import models
-
-# Create your models here.
->>>>>>> 719cd225de0c04018c059ed171d0c81cd36fa789
--- main/supervision/online_monitoring/crawler/googles.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  6 01:35:31 2023
-
-@author: kimse
-"""
-
